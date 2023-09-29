d2bb7937ad02a9c43930df99aa2f0edc772ca990
jharro <jharro@gmail.com>
2023-09-28 19:19:36 -0500

    mejoras

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,20 @@
-let boton=document.getElementById('cambiarFondo')
+//let boton=document.getElementById('cambiarFondo')
 
-boton.addEventListener('click',()=>{
+
+/*boton.addEventListener('click',()=>{
     document.body.style.backgroundColor='fuchsia'
 })
+*/
+
+
+function mostrar(){
+    document.getElementById('texto').style.display = 'block';
+    document.getElementById('mostrar').style.display='none'
+    document.getElementById('ocultar').style.display='inline'
+}
+
+function ocultar(){
+    document.getElementById('texto').style.display = 'none';
+    document.getElementById('mostrar').style.display='inline'
+    document.getElementById('ocultar').style.display='none'
+}
